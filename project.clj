7c1843a7ffd3b5d345ba67c48aad1c75d52f9a3d
Luis Gabriel Lima <luis.gabriel@openbossa.org>
2013-03-31 16:41:56 -0300

    Initial commit
    Default project generated by lein.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject tsearch "0.0.1-SNAPSHOT"
+  :description ""
+  :url "https://github.com/luisgabriel/tsearch-clj"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main tsearch.core)
---
+++ test/tsearch/core_test.clj
@@ -0,0 +1,7 @@
+(ns tsearch.core-test
+  (:use clojure.test
+        tsearch.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
