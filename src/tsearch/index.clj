12c2b147e2891a8dfb3cf10b615df81e57ac025e
Luís Gabriel Lima <lampih@gmail.com>
2013-04-05 15:30:03 -0300

    Splitting the index by the first letter of the words
    Now the index layout is really similar to the haskell version.
    The index is an array where each position represents a letter and
    contains a hash-map with all words on the vocabulary which starts with
    this letter.

--- src/tsearch/index.clj
+++ src/tsearch/index.clj
@@ -1,23 +1,36 @@
 (ns tsearch.index)
 
-(defn empty-index [] {:index (hash-map) :nfiles 0 :id 0 })
+(def zero (Character/getNumericValue 0))
+
+(defn- dord [ch]
+  (- (Character/getNumericValue ch) zero))
+
+(defn empty-index []
+  (let[index (vec (repeat (+ (dord \z) 1) (hash-map)))]
+    (dorun index)
+    { :index index :nfiles 0 :id 0 }))
 
 (defn insert [oc-pair index-obj]
-  (let [file-path (first oc-pair)
+  (let [filep (first oc-pair)
         occurrences (seq (nth oc-pair 1))
         index (:index index-obj)
         file-counter (:nfiles index-obj)
         id (:id index-obj)]
-    (loop [ocs occurrences new-index index]
+    (loop [ocs occurrences ind index]
       (if (empty? ocs)
-        {:index new-index :nfiles (+ file-counter 1) :id id}
+        {:index ind :nfiles (+ file-counter 1) :id id}
 
         (let [oc-pair (first ocs)
               word (key oc-pair)
-              positions (val oc-pair)]
-          (recur (rest ocs) (merge-with concat new-index (hash-map word (list [file-path positions])))))))))
+              positions (val oc-pair)
+              voc-pos (dord (first word))
+              voc (nth ind voc-pos)
+              new-voc (update-in voc [word] #(conj % [filep positions]))
+              new-index (assoc ind voc-pos new-voc)]
+          (recur (rest ocs) new-index))))))
 
 (defn find-oc [word index-obj]
   (let [index (:index index-obj)
-        result (find index word)]
+        voc (nth index (dord (first word)))
+        result (find voc word)]
     (if result (val result) '())))
