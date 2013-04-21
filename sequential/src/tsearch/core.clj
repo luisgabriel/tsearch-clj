1a6ea8e107bbf083cdddcdaf1e6164dc3cac9ab2
Luís Gabriel Lima <lampih@gmail.com>
2013-04-21 17:04:01 -0300

    [Sequential] Allow more than one query per execution
    Also formatting the output properly.

--- sequential/src/tsearch/core.clj
+++ sequential/src/tsearch/core.clj
@@ -5,6 +5,11 @@
   (:require [tsearch.query :as query])
   (:gen-class))
 
+(defn print-result [result]
+  (def ordered-result (sort-by (fn [e] [(/ 1.0 (nth e 1)) (first e)]) result))
+  (doseq [pair ordered-result]
+    (println "File: " (first pair) "    Occurrences: " (nth pair 1))))
+
 (defn occurrences [file]
   (let [content (slurp file)
         occurrences (lexer/process-content content)]
@@ -23,6 +28,10 @@
         (let [ocs (occurrences (first fs))]
           (recur (index/insert ocs index) (rest fs))))))
 
-  (println (str "Path: " (first args)))
-  (println (pr-str (query/perform (query/parseq (nth args 1)) global-index)))
+  (doseq [raw-query (rest args)]
+    (let [query (query/parseq raw-query)
+          result (query/perform query global-index)]
+      (println (str "RESULT for \"" raw-query "\":"))
+      (print-result result)
+      (println "")))
 )
