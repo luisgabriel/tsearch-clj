9931dd0930f1757240ea3928a9273d0291b5657b
Luís Gabriel Lima <lampih@gmail.com>
2013-04-28 10:56:37 -0300

    Make the number of indexing threads an argument

--- src/tsearch/core.clj
+++ src/tsearch/core.clj
@@ -8,11 +8,14 @@
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
 
-  (def nworkers 3)  ; number of indexing threads
+  (def dir-path (first args))
+  (def nworkers (read-string (nth args 1)))  ; number of indexing threads
+  (def raw-queries (rest (rest args)))
+
   (def nindices 4)  ; initial sub-indices
   (def max-files 3) ; max number of files indexed per sub-index
 
-  (def files (scanner/all-files (first args)))
+  (def files (scanner/all-files dir-path))
 
   (engine/process-files nindices max-files nworkers files)
-  (engine/process-search (rest args)))
+  (engine/process-search raw-queries))
